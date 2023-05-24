3972f9c38b87ee828ac31d2e304c567ec31f679c
GordeychikStepan <89136986+GordeychikStepan@users.noreply.github.com>
2023-05-24 10:08:11 +0500

    Add files via upload

---
+++ Зачет по ОА/WindowsFormsApp1/Form1.Designer.cs
@@ -0,0 +1,122 @@
+﻿
+namespace WindowsFormsApp1
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose (bool disposing)
+        {
+            if ( disposing && (components != null) )
+            {
+                components.Dispose( );
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent ()
+        {
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.button1 = new System.Windows.Forms.Button();
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(0, 132);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.ReadOnly = true;
+            this.dataGridView1.Size = new System.Drawing.Size(598, 193);
+            this.dataGridView1.TabIndex = 0;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(12, 35);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(90, 23);
+            this.button1.TabIndex = 3;
+            this.button1.Text = "Отобразить";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.displayButton_Click);
+            // 
+            // checkBox1
+            // 
+            this.checkBox1.AutoSize = true;
+            this.checkBox1.Location = new System.Drawing.Point(12, 12);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(194, 17);
+            this.checkBox1.TabIndex = 4;
+            this.checkBox1.Text = "Сортировка по названию страны";
+            this.checkBox1.UseVisualStyleBackColor = true;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(108, 35);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(90, 23);
+            this.button2.TabIndex = 5;
+            this.button2.Text = "Удалить";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.deleteButton_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label1.Location = new System.Drawing.Point(8, 109);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(105, 20);
+            this.label1.TabIndex = 6;
+            this.label1.Text = "Все путевки:";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.Info;
+            this.ClientSize = new System.Drawing.Size(598, 337);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.checkBox1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.dataGridView1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Путевки";
+            this.Load += new System.EventHandler(this.MainForm_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.CheckBox checkBox1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ Зачет по ОА/WindowsFormsApp1/Form1.cs
@@ -0,0 +1,170 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.IO;
+using System.Linq;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp1
+{
+	public partial class MainForm : Form
+	{
+		private List<Country> countries;
+		private List<Hotel> hotels;
+
+		public MainForm()
+		{
+			InitializeComponent();
+		}
+
+		private void MainForm_Load (object sender, EventArgs e)
+		{
+			// Загрузка данных из файлов
+			countries = LoadCountriesFromFile("countries.txt");
+			hotels = LoadHotelsFromFile("hotels.txt");
+
+			if ( countries.Count == 0 || hotels.Count == 0 )
+			{
+				MessageBox.Show("Файлы не содержат данных.");
+				return;
+			}
+		}
+
+		private void displayButton_Click (object sender, EventArgs e)
+		{
+			DisplayData();
+		}
+
+		private void deleteButton_Click (object sender, EventArgs e)
+		{
+			if (dataGridView1.SelectedRows.Count > 0)
+			{
+				int recordId = (int) dataGridView1.SelectedRows [ 0 ].Cells [ 0 ].Value;
+
+				countries.RemoveAll(country => country.Id == recordId);
+				hotels.RemoveAll(hotel => hotel.Id == recordId);
+
+				int sortParameter = GetSortParameter( );
+				var updatedResult = from country in countries
+									join hotel in hotels on country.Id equals hotel.Id
+									orderby GetOrderBy(sortParameter, country)
+									select new
+									{
+										Pid = country.Id,
+										HotelName = hotel.Name,
+										Country = country.Name,
+										Quantity = hotel.Type.Length,
+										Price = hotel.Type.Length * hotel.Price
+									};
+
+				dataGridView1.DataSource = null;
+				dataGridView1.DataSource = updatedResult.ToList( );
+				SetColumnsHeader();
+
+				MessageBox.Show("Запись успешно удалена.", "Внимание!");
+			}
+			else MessageBox.Show("Выберите запись для удаления.", "Внимание!");
+		}
+
+		private void DisplayData()
+		{
+			int sortParameter = GetSortParameter();
+
+			var result = from country in countries
+						 join hotel in hotels on country.Id equals hotel.Id
+						 orderby GetOrderBy(sortParameter, country)
+						 select new
+						 {
+							 Pid = country.Id,
+							 HotelName = hotel.Name,
+							 Country = country.Name,
+							 Quantity = hotel.Type.Length,
+							 Price = hotel.Type.Length * hotel.Price
+						 };
+
+			dataGridView1.DataSource = result.ToList();
+			SetColumnsHeader();
+		}
+
+		private void SetColumnsHeader()
+		{
+			dataGridView1.Columns [ 0 ].HeaderText = "Идентификатор путевки";
+			dataGridView1.Columns [ 1 ].HeaderText = "Название отеля";
+			dataGridView1.Columns [ 2 ].HeaderText = "Страна";
+			dataGridView1.Columns [ 3 ].HeaderText = "Количество";
+			dataGridView1.Columns [ 4 ].HeaderText = "Цена";
+		}
+
+		private List<Country> LoadCountriesFromFile (string filePath)
+		{
+			List<Country> countries = new List<Country>();
+
+			if (!File.Exists(filePath))
+			{
+				MessageBox.Show($"Файл {filePath} не существует.");
+				return countries;
+			}
+
+			try
+			{
+				string [ ] lines = File.ReadAllLines(filePath);
+
+				foreach ( string line in lines )
+				{
+					string [ ] parts = line.Split(';');
+					int id = int.Parse(parts [ 0 ]);
+					string name = parts [ 1 ];
+					countries.Add(new Country { Id = id, Name = name });
+				}
+				return countries;
+			} catch(Exception ex) { MessageBox.Show(ex.ToString()); Application.Exit( ); return null; }
+			
+		}
+
+		private List<Hotel> LoadHotelsFromFile (string filePath)
+		{
+			List<Hotel> hotels = new List<Hotel>();
+
+			if (!File.Exists(filePath))
+			{
+				MessageBox.Show($"Файл {filePath} не существует.");
+				return hotels;
+			}
+
+			string [] lines = File.ReadAllLines(filePath);
+
+			try
+			{
+				foreach ( string line in lines )
+				{
+					string [ ] parts = line.Split(';');
+					string type = parts [ 0 ];
+					int id = int.Parse(parts [ 1 ]);
+					string name = parts [ 2 ];
+					int price = int.Parse(parts [ 3 ]);
+					hotels.Add(new Hotel { Type = type, Id = id, Name = name, Price = price });
+				}
+				return hotels;
+			} catch ( Exception ex ) { MessageBox.Show(ex.ToString( )); Application.Exit( ); return null; }
+		}
+
+		private int GetSortParameter()
+		{
+			int sortParameter = 1; // По умолчанию сортировка по идентификатору
+
+			if (checkBox1.Checked)
+			{
+				sortParameter = 2; // Сортировка по названию страны
+			}
+
+			return sortParameter;
+		}
+
+		private object GetOrderBy(int sortParameter, Country country)
+		{
+			if (sortParameter == 1) { return country.Id; }
+			else { return country.Name; }
+		}
+
+	}
+}
